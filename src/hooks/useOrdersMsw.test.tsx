aeab097d70dd5f867a4f17d28b02daa8de5b7a55
klich84 <klich84@gmail.com>
2025-08-06 15:34:04 -0500

    test(msw): ❌ simulate 500 error tests in endpoints with Mock Service Worker

--- src/hooks/useOrdersMsw.test.tsx
+++ src/hooks/useOrdersMsw.test.tsx
@@ -5,6 +5,8 @@ import { SessionProvider, useSession } from '../context/AuthContext'
 import { MemoryRouter } from 'react-router-dom'
 import { act, renderHook, waitFor } from '@testing-library/react'
 import useOrders from './useOrders'
+import { http, HttpResponse } from 'msw'
+import { server } from '../mocks/server'
 
 vi.mock('../context/AuthContext', async () => {
   const actual = await vi.importActual('../context/AuthContext')
@@ -39,8 +41,30 @@ describe('useOrdersMSW', () => {
     const lengthOrders = result.current.orders.length
 
     await waitFor(() => {
-      expect(initialLoading).toBe(true)
       expect(lengthOrders).toBe(1)
+    })
+  })
+
+  it('Debe obtener un error', async () => {
+    server.use(
+      http.get('http://localhost:3001/orders', () => {
+        return new HttpResponse(null, {
+          status: 500,
+          statusText: 'Internal Server Error',
+        })
+      })
+    )
+
+    const { result, rerender } = renderHook(() => useOrders(), { wrapper: wrapper })
+    const initialLoading = result.current.loading
+
+    expect(initialLoading).toBe(true)
+
+    await act(async () => rerender())
+
+    await waitFor(() => {
+      const error = result.current.error
+      expect(error).toBe('Failed to fetch orders. Please try again later.')
     })
   })
 })
